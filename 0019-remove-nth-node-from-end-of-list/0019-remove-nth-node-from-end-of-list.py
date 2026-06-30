97ebae52bfba01914418effbd7e3f232fec428e4
Selina Wu <144982441+selinaaawu@users.noreply.github.com>
2026-06-30 00:16:48 -0400

    Time: 0 ms (100%), Space: 19.2 MB (63.47%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -8,7 +8,7 @@ class Solution:
         
         ## LINKED LIST | time: O(n), space: O(1)
         # use dummy pointer since head can be removed
-        # left pointer starts dummy, right poiner starts head
+        # left, right pointer starts dummy
         # move right pointer n steps ahead
         # move both pointers together until right pointer reaches end
         # left pointer right before node to be removed
@@ -17,12 +17,12 @@ class Solution:
         dummy = ListNode(0, head)   
         
         # move right pointer n times
-        left, right = dummy, head
+        left, right = dummy, dummy
         for _ in range(n):
             right = right.next
         
         # move both pointers until right reaches end
-        while right:
+        while right.next:
             left = left.next
             right = right.next
         
